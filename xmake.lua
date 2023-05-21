aeb63d237304263a19e199b1b9abf65c51aff7bb
Mrowr Purr <mrowr.purr@gmail.com>
2023-05-21 14:50:16 -0700

    Support for non-MSVC (and fix .h path)

--- xmake.lua
+++ xmake.lua
@@ -1,13 +1,12 @@
 set_languages("c++20")
 add_rules("mode.debug")
-set_toolchains("msvc")
 
 -- Required for the example, but you can bring your own logger and add an adapter
 add_requires("spdlog")
 
 target("_Log_")
     set_kind("headeronly")
-    add_headerfiles("include/_Log_")
+    add_headerfiles("include/_Log_.h")
     add_includedirs("include", {public = true})
 
 target("Example")
