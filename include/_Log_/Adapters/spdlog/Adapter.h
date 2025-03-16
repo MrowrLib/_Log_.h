e5a205470d634e237f011046c0fad338cccd426e
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-16 13:28:46 -0700

    Use spdlog::default_logger() if it exists

--- include/_Log_/Adapters/spdlog/Adapter.h
+++ include/_Log_/Adapters/spdlog/Adapter.h
@@ -28,12 +28,30 @@ namespace _Log_ {
                 std::string                     _logFilePath;
 
                 void ReconfigureSpdlogger() {
+                    // If there is a default logger, use it
+                    // unless a custom log file path has been provided.
+                    //
+                    // This means you won't get a "console" logger as the
+                    // default logger if spdlog::default_logger() is present.
+                    if (_logFilePath.empty()) {
+                        if (auto defaultLogger = spdlog::default_logger()) {
+                            _spdlogLogger = defaultLogger;
+                            return;
+                        }
+                    }
+
+                    // Drop the default logger if it exists
                     if (_spdlogLogger) spdlog::drop("basic_logger");
+
+                    // Setup the logger (either to console or to file)
                     if (_logFilePath.empty()) _spdlogLogger = spdlog::stderr_color_mt("console");
                     else
                         _spdlogLogger = spdlog::basic_logger_mt("basic_logger", _logFilePath, true);
+
                     _spdlogLogger->set_level(spdlog::level::trace);
                     _spdlogLogger->flush_on(spdlog::level::trace);
+
+                    // Default to the very very simplest of patters:
                     _spdlogLogger->set_pattern("%v");
                 }
 
